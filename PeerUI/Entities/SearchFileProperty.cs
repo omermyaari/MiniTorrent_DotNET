9e663d9f8088fb7a13e573675ab33f38e75a4db0
Omer Yaari <omermyaari@gmail.com>
2017-03-01 16:50:24 +0200

    added search button click event

---
+++ PeerUI/Entities/SearchFileProperty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeerUI.Entities {
+    public class SearchFileProperty {
+        public string Name {
+            get; set;
+        }
+        public long Size {
+            get; set;
+        }
+        public int Peers {
+            get; set;
+        }
+
+        public SearchFileProperty(string Name, long Size, int Peers) {
+            this.Name = Name;
+            this.Size = Size;
+            this.Peers = Peers;
+        }
+    }
+}
--- PeerUI/MainWindow.xaml.cs
+++ PeerUI/MainWindow.xaml.cs
@@ -1,22 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Net;
-using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
 using System.Threading;
+using PeerUI.Entities;
 
 namespace PeerUI {
     /// <summary>
@@ -207,6 +196,12 @@ namespace PeerUI {
                 sizeChangedEventArgs.Handled = true;
                 ((GridViewColumnHeader)sender).Column.Width = 60;
             }
+        }
+
+        private void buttonSearch_Click(object sender, RoutedEventArgs e) {
+            List<SearchFileProperty> items = new List<SearchFileProperty>();
+            items.Add(new SearchFileProperty("Fuck", 15, 15));
+            listViewSearch.ItemsSource = items;
         }
     }
 }
